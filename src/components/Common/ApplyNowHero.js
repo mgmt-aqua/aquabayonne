2f435b1a01da8dc214a5ca57f7fdde446dd17aa6
Raj Shah <rsshah1112@gmail.com>
2025-03-27 17:26:31 -0400

    started work on the neighborhood information page

--- src/components/Common/ApplyNowHero.js
+++ src/components/Common/ApplyNowHero.js
@@ -1,26 +1,14 @@
 import * as React from 'react';
 import './ApplyNowHero.css'
-import { motion, AnimatePresence } from 'framer-motion';
-import { Button, Row, Col } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
 
 export default function ApplyNowHero() {
     
     return (
-        <AnimatePresence>
         <div className="apply-now-hero">
-            <h1 className="apply-now-hero-text">Your Perfect Apartment Awaits – Start the Search Now</h1>
+            <h1 className="apply-now-hero-text">Ready to Move In? Browse Our Available Apartments and Apply Today!</h1>
             <Button className='apply-now-hero-button'>Apply Now</Button>
-            {/* <Row>
-            <Col key={1} xs={12} sm={12} md={6} lg={6} className="mb-4 apply-now-hero-text-col">
-                
-            </Col>
-            <Col key={2} xs={12} sm={12} md={6} lg={6} className="mb-4 apply-now-hero-button-col">
-                
-            </Col>
-            </Row> */}
         </div>
-        </AnimatePresence>
-        
     );
 }
--- src/components/Neighborhood/NeighborhoodInformationPage.js
+++ src/components/Neighborhood/NeighborhoodInformationPage.js
@@ -3,14 +3,15 @@ import FramerSlide from '../Common/FramerSlide'
 import InformationPage from '../Common/InformationPage';
 import { desktopStyles, mobileStyles } from '../../configuration/framer-slide-styles';
 
-import './NeighborhoodInformationPage'
+import coverImage from '../../img/neighborhood_cover.jpg'
+import './NeighborhoodInformationPage.css'
 
 export default function NeighborhoodInformationPage() {
 
     return (
         <div className="neighborhood-information-page">
             <FramerSlide text="Our Neighborhood" desktopTextStyles={desktopStyles} mobileTextStyles={mobileStyles} />
-            <InformationPage pageTitle={'Our Neighborhood'}></InformationPage>
+            <InformationPage pageTitle={'Our Neighborhood'} img={coverImage} imgStyles={'neighborhood-cover-img'}></InformationPage>
         </div>
     );
 }
