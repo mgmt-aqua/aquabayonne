d4cfde49239e77f8ad7bc6e3fc88f9fee6bce24d
Raj Shah <rsshah1112@gmail.com>
2025-04-01 20:43:59 -0400

    footer v3

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,74 +11,29 @@ export default function Footer({ id }) {
     const content = (
         <footer className="bg-dark text-white py-4 d-flex align-items-center justify-content-center footer-container">
             <Container fluid className="text-center px-0">
-            <div className="d-flex justify-content-center gap-5">
-                            <img 
-                                src={logo} 
-                                alt="Aqua Bayonne logo" 
-                                className="mb-3 footer-logo" 
-                            />
-                            <img 
-                                src={equalHousingLogo} 
-                                alt="Equal Housing logo" 
-                                className="mb-3 footer-logo" 
-                            />
-                            
-                        </div>
-                <Row>
-                    <Col xs={12} sm={12} md={4} lg={4} className="footer-visit-us">
-                        <h3 className="footer-title">Visit Us</h3>
-                        <h3 className="footer-title-content">54 Flagship Street, Bayonne, NJ 07002</h3>
-                    </Col>
-                    <Col xs={12} sm={12} md={4} lg={4} className="footer-contact-us">
-                        <h3 className="footer-title">Contact Us</h3>
-                        <h3 className="footer-contact-us-content">Call us at <a href="tel:5555555555">(555) 555-555</a> or <Link to="/contact">reach out.</Link></h3>
-                    </Col>
-                    <Col xs={12} sm={12} md={4} lg={4} className="footer-follow-us">
-                        <h3 className="footer-title">Follow Us</h3>
-                        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-                                <FaFacebook size={30} className="footer-icon" />
-                            </a>
-                            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-                                <FaInstagram size={30} className="footer-icon" />
-                            </a>
-                            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-                                <FaTwitter size={30} className="footer-icon" />
-                            </a>
-                            <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer" aria-label="TikTok">
-                                <FaTiktok size={30} className="footer-icon" />
-                            </a>
-                    </Col>
-                    <Row>
-                        <p className="footer-rights">
-                            Copyright ©2025 Aqua Bayonne. All Rights Reserved. All trademarks are the property of their respective holders. <Link to="/privacy" className="privacy-policy-link">PRIVACY POLICY.</Link>
-                        </p>
-
-                    </Row>
+                <Row className="d-flex justify-content-center align-items-center g-0">
                     {/* First Column with Inline Logos */}
-                    {/* <Col xs={12} sm={12} md={4} lg={4} className="footer-col d-flex flex-column align-items-center justify-content-center">
+                    <Col xs={12} sm={12} md={4} lg={4} className="footer-col d-flex flex-column align-items-center justify-content-center">
                         <div className="d-flex justify-content-center gap-5">
-                            <img 
-                                src={logo} 
-                                alt="Aqua Bayonne logo" 
-                                className="mb-3 footer-logo" 
+                            <img
+                                src={logo}
+                                alt="Aqua Bayonne logo"
+                                className="mb-3 footer-logo"
                             />
-                            <img 
-                                src={equalHousingLogo} 
-                                alt="Equal Housing logo" 
-                                className="mb-3 footer-logo" 
+                            <img
+                                src={equalHousingLogo}
+                                alt="Equal Housing logo"
+                                className="mb-3 footer-logo"
                             />
                         </div>
-                    </Col> */}
-                    
+                    </Col>
+
                     {/* Second Column with Address, Contact Info, and Hours */}
-                    {/* <Col xs={12} sm={12} md={4} lg={4} className="footer-col d-flex flex-column align-items-center justify-content-center">
+                    <Col xs={12} sm={12} md={4} lg={4} className="footer-col d-flex flex-column align-items-center justify-content-center">
                         <p className="footer-address">54 Flagship Street, Bayonne, NJ 07002</p>
 
-                        <p className="footer-phone-number">
-                            <a href="tel:+12015610066">Phone: (201) 561-0066</a>
-                        </p>
-                        <p className="footer-email">
-                            <a href="mailto:leasing@aquabayonne.com">Email: leasing@aquabayonne.com</a>
+                        <p className="footer-contact-information">
+                            <a href="tel:+12015610066">(201) 561-0066</a> | <a href="mailto:leasing@aquabayonne.com">leasing@aquabayonne.com</a>
                         </p>
 
                         <div className="footer-hours">
@@ -86,13 +41,11 @@ export default function Footer({ id }) {
                             <p>Sun: 10 am - 5:30 pm</p>
                         </div>
 
-                        <p className="footer-rights">
-                            Copyright ©2025 Aqua Bayonne. All Rights Reserved. All trademarks are the property of their respective holders. <Link to="/privacy" className="privacy-policy-link">PRIVACY POLICY.</Link>
-                        </p>
-                    </Col> */}
+                       
+                    </Col>
 
                     {/* Third Column with Social Icons */}
-                    {/* <Col xs={12} sm={12} md={4} lg={4} className="footer-col d-flex flex-column align-items-center justify-content-center">
+                    <Col xs={12} sm={12} md={4} lg={4} className="footer-col d-flex flex-column align-items-center justify-content-center">
                         <div className="d-flex justify-content-center gap-3">
                             <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
                                 <FaFacebook size={30} className="footer-icon" />
@@ -107,8 +60,11 @@ export default function Footer({ id }) {
                                 <FaTiktok size={30} className="footer-icon" />
                             </a>
                         </div>
-                    </Col> */}
+                    </Col>
                 </Row>
+                 <p className="footer-rights">
+                            Copyright ©2025 Aqua Bayonne. All Rights Reserved. All trademarks are the property of their respective holders. <Link to="/privacy" className="privacy-policy-link">PRIVACY POLICY.</Link>
+                        </p>
             </Container>
         </footer>
     );
